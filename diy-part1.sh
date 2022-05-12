036b40d0aff745ccfee4024a1cbc000b7b2586a6
qiruizheng <4271439+qiruizheng@users.noreply.github.com>
2022-05-12 19:52:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,2 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git packages https://git.openwrt.org/feed/packages' >>feeds.conf.default
-echo 'src-git luci https://git.openwrt.org/project/luci.git' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
